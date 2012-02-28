42c5aed9993ea613f13c6000c2ede0ccc1a581aa
Jeff Horak <jeff.horak@gmail.com>
2012-02-27 22:45:46 -0500

    Some site improvements

--- siteContent/js/script.js
+++ siteContent/js/script.js
@@ -1,6 +1,39 @@
 /* Author: Jeff Horak
 */
 
+var kilius = (function() {
+  return {
+    createListItem: function(sLink, lLink, h) {
+      var li = document.createElement('li'),
+          div = document.createElement('div'),
+          copy = document.createElement('span'),
+          shortLink = document.createElement('a'),
+          longLink = document.createElement('a'),
+          hits = document.createElement('span');
+
+      li.setAttribute('class', 'historyLink');
+      li.appendChild(div);
+      div.setAttribute('class', 'shortContainer');
+      div.appendChild(copy);
+      copy.setAttribute('class', 'copyLink');
+      copy.appendChild(document.createTextNode('Copy'));
+      div.appendChild(shortLink);
+      shortLink.setAttribute('href', sLink);
+      shortLink.setAttribute('class', 'shortLink');
+      shortLink.appendChild(document.createTextNode(sLink.substring(sLink.indexOf('kili'))));
+      li.appendChild(longLink);
+      longLink.setAttribute('href', lLink);
+      longLink.setAttribute('class', 'longLink');
+      longLink.appendChild(document.createTextNode(lLink));
+      li.appendChild(hits);
+      hits.setAttribute('class', 'hitCount');
+      hits.appendChild(document.createTextNode(h));
+
+      return li;
+    }
+  };
+})();
+
 $(document).ready(function() {
   // Register the click handler for the submit button
   $('#submit-btn').click(function() {
@@ -15,9 +48,21 @@ $(document).ready(function() {
     oXHR.open('POST', '/service/shorten', true);
     oXHR.setRequestHeader('Content-Type', 'application/json');
     oXHR.onreadystatechange = function(oEvent) {
+      var resp = {},
+          li = null,
+          ul = null;
+
       if (oXHR.readyState === 4) {
         if (oXHR.status === 200) {
-          console.log("Success " + oXHR.responseText);
+          resp = JSON.parse(oXHR.responseText);
+          li = kilius.createListItem(resp.url, url.value, 7);
+          ul = document.getElementById('shortList');
+
+          // Insert as the new first item in the list
+          if (ul && li) {
+            ul.insertBefore(li, ul.firstChild);
+          }
+
         } else {
           console.log("Error " + oXHR.status + " " + oXHR.statusText);
         }
