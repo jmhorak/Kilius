a59c1e33cf3787209a65b7bbca093db67d1608ec
Jeff Horak <jeff.horak@gmail.com>
2012-03-06 23:08:44 -0500

    Added logging and error handling when shortening a URL

--- server/kilius.js
+++ server/kilius.js
@@ -14,17 +14,27 @@ var http = require('http'),
     server = null;
 
 var Kilius = function() {
-  var that = this;
+  var that = this,
+      blackList = [],
+      doLog = function(collection, payload) {
+          collection.insert(payload, {safe: false});
+      };
+
+  // Check for black-listed clients
+  this.isBlackListed = function(client) {
+    return !!blackList[client];
+  };
 
   // Serve HTML
   this.servePageFor = function(client, page, response) {
     fs.readFile(page, function(err, fd) {
       if (err) {
-        log(client, 'Could not load ' + page);
+        that.logError(client, 'Could not load ' + page, 503);
         response.writeHead(503); // 503 => Service temporarily unavailable
         response.end();
       } else {
         // Write the page to the response stream
+        that.log(client, 'Served ' + page);
         response.writeHead(200);
         response.end(fd);
       }
@@ -34,10 +44,11 @@ var Kilius = function() {
   this.serveResourceFor = function(client, resource, response) {
     fs.readFile(resource, function(err, fd) {
       if (err) {
-        log(client, ['Invalid resource ', resource, ' requested'].join(''));
+        that.logError(client, ['Invalid resource ', resource, ' requested'].join(''), 404);
         response.writeHead(404); // 404 => Not Found
         response.end();
       } else {
+        that.log(client, 'Served ' + resource);
         switch (true) {
           case /\.css$/i.test(resource):
             response.writeHead(200, { 'Content-Type': 'text/css' });
@@ -68,11 +79,12 @@ var Kilius = function() {
     fs.readFile(path, function(err, fd) {
       if (err) {
         // This isn't a terrible problem, we'll just return nothing
-        log(client, 'Could not load the favicon');
+        that.logError(client, 'Could not load the favicon');
         response.writeHead(200);
         response.end();
       } else {
         // Return with the correct MIME type
+        that.log(client, "Served fav icon");
         response.writeHead(200, {'Content-Type': 'image/x-icon'});
         response.end(fd);
       }
@@ -83,16 +95,26 @@ var Kilius = function() {
     var data = [];
     request.on('data', function(chunk) {
       data.push(chunk);
-      console.log('Received: ' + chunk);
     }).on('end', function() {
       callback(JSON.parse(data));
     });
   };
 
   this.log = function(client, msg) {
-    console.log([
-      '[', client.toString(), '] ', new Date(), ': ', msg
-    ].join(''));
+    doLog(mongo.collection('actLog'), {
+      client: client,
+      date: new Date(),
+      msg: msg
+    });
+  };
+
+  this.logError = function(client, msg, code) {
+    doLog(mongo.collection('errLog'), {
+      client: client,
+      date: new Date(),
+      msg: msg,
+      code: code
+    });
   };
 
   // Start the server
@@ -101,6 +123,16 @@ var Kilius = function() {
       var servURL = urlService.parse(req.url),
           path = servURL.pathname,
           host = req.headers.host;
+
+      that.log(host, 'Starting to process request');
+
+      if (that.isBlackListed(host)) {
+        // This user is not allowed
+        that.logError(host, 'Blacklisted client attempting to connect', 403);
+        res.writeHead(403); // Forbidden
+        res.end();
+        return;
+      }
 
       if (path.charAt(0) === ':') {
         path.substring(1);
@@ -120,13 +152,33 @@ var Kilius = function() {
           that.getRequestData(req, function(data) {
             data.database = mongo;
             data.clientID = host;
-            sh.shorten(data, function(url) {
+            sh.shorten(data, function(result) {
+              that.log(host, ['Finished shortening URL as [', result.url, '] ',
+                (24 - result.throttle).toString(), ' more requests available today'].join(''));
+
               res.writeHead(200, { 'Content-Type': 'application/json'});
-              res.end(JSON.stringify({ url: url }));
+              res.end(JSON.stringify({
+                url: result.url,
+                throttle: result.throttle
+              }));
+            }, function(err) {
+              // TODO: Should this return 500 instead?
+              res.writeHead(200, { 'Content-Type': 'application/json'});
+              res.end(JSON.stringify({
+                isErr: true,
+                errCode: err.code,
+                errMessage: err.clientMessage
+              }));
+
+              // Remove the client information before writing to the error log
+              delete err.clientMessage;
+
+              that.logError(host, JSON.stringify(err), 200);
             });
           });
         } else {
           // Service not found
+          that.logError(host, 'Service not implemented', 501);
           res.writeHead(501); // 501 => Not Implemented
           res.end();
         }
@@ -137,8 +189,7 @@ var Kilius = function() {
         that.serveResourceFor(host, '../siteContent/' + path, res);
       }
     } catch (e) {
-      that.log('Kilius Server', 'createServer: Exception thrown: ' + e.toString());
-
+      that.logError('Kilius Server', 'createServer: Exception thrown: ' + e.toString(), 500);
       res.writeHead(500); // 500 => Internal Server Error
       res.end();
     }
@@ -149,7 +200,9 @@ var Kilius = function() {
 mongo = new m.Db('kilius', new m.Server('localhost', 27017, {}));
 
 mongo.open(function(err, result) {
-  var throwOnErr = function(err) { if (err) { throw err; } };
+  // If any database initialization method fails, throw
+  var throwOnErr = function(err) { if (err) { throw err; } },
+      mb = 1048576;
 
   throwOnErr(err);
 
@@ -170,5 +223,18 @@ mongo.open(function(err, result) {
       mongo.createCollection('links', {safe: true}, function(err) { throwOnErr(err); });
     }
   });
+  mongo.collection('errLog', {safe: true}, function(err, result) {
+    if (err) {
+      // Create the err log collection, cap it at 10 MB
+      mongo.createCollection('errLog', {safe: true, capped: true, size: mb*10}, function(err) { throwOnErr(err); });
+    }
+  });
+  mongo.collection('actLog', {safe: true}, function(err, result) {
+    if (err) {
+      // Create the activity log collection, cap it at 50 MB
+      mongo.createCollection('actLog', {safe: true, capped: true, size: mb*50}, function(err) { throwOnErr(err); });
+    }
+  });
+
   server = new Kilius();
 });
